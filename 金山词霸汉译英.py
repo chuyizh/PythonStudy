7d639a9a6e8c95c36fea0288854decca6f2a6185
jwmycz <924445155@qq.com>
2018-12-12 21:52:34 +0800

    Update 金山词霸汉译英.py

--- 金山词霸汉译英.py
+++ 金山词霸汉译英.py
@@ -1,5 +1,3 @@
-```python
-
 import urllib.request
 import urllib.parse
 import json
@@ -13,5 +11,3 @@ data=response.read().decode('utf8')
 result=json.loads(data)
 dest=result['content']['out']
 print('翻译的结果为：'+ dest)
-
-```
