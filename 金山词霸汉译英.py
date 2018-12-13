cbb49cb1be751f52e2e3a76b7630fda84870a7ab
jwmycz <924445155@qq.com>
2018-12-13 14:52:37 +0800

    Add：User-Agent
    增加：User-Agent

--- 金山词霸汉译英.py
+++ 金山词霸汉译英.py
@@ -6,7 +6,9 @@ inputs=input('请输入需要翻译的中文：')
 postdata={'f':'auto','t':'auto','w':inputs}
 postdata=urllib.parse.urlencode(postdata)
 postdata=bytes(postdata,encoding='utf8')
-response=urllib.request.urlopen(fanyi,data=postdata)
+request=urllib.request.Request(fanyi)
+request.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36')
+response=urllib.request.urlopen(request,data=postdata)
 data=response.read().decode('utf8')
 result=json.loads(data)
 dest=result['content']['out']
