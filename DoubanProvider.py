6498170d9a381c8acd98b7094fd2b7cd3b572946
jwmycz <924445155@qq.com>
2019-01-05 11:21:53 +0800

    Update DoubanProvider.py

--- DoubanProvider.py
+++ DoubanProvider.py
@@ -5,5 +5,6 @@ url='https://read.douban.com/provider/all'
 data=urllib.request.urlopen(url).read().decode('utf8')
 redata='<div class=\"name\">(.*?)</div><div class=\"works-num\">(.*?)</div>'
 rst=re.compile(redata).findall(data)
-for i in range(0,len(rst)):
-    print(rst[i])
+with open('c1c.txt','w') as f:
+    f.write('\n'.join('{}{}'.format(x[0],x[1])for x in rst))
+   #f.write('\n'.join('%s %s' % x for x in rst))
