7adb7cf81533fcc0349bd58206e73570cee76571
jwmycz <924445155@qq.com>
2019-01-05 12:14:56 +0800

    Update DoubanMovies.py

--- DoubanMovies.py
+++ DoubanMovies.py
@@ -2,7 +2,7 @@ import urllib.request
 import re
 url='https://movie.douban.com/chart'
 data=urllib.request.urlopen(url).read().decode('utf8')
-redata=open('cc.txt','r',encoding='utf8').read()
+redata=open('DoubanMoviesRe.txt','r',encoding='utf8').read()
 rst=re.compile(redata).findall(data)
 with open('chart.csv','w') as f:
     f.write('\n'.join('%s %s %s %s %s %s %s %s %s %s %s' % x for x in rst))
