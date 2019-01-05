95bf086956e21711cd9cc4191e4f56aa7ae92762
jwmycz <924445155@qq.com>
2019-01-05 17:36:58 +0800

    修改文件写入错误

--- QiushibaikeText.py
+++ QiushibaikeText.py
@@ -4,10 +4,11 @@ headers=('User-Agent','Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.
 opener=urllib.request.build_opener()
 opener.addheaders=[headers]
 urllib.request.install_opener(opener)
-for i in range(1,100):
+f=open('xiushibaike.txt', 'w',encoding='utf-8')
+for i in range(1,50):
     url='https://www.qiushibaike.com/text/page/'+str(i)+'/'
     data=urllib.request.urlopen(url).read().decode('utf8')
-    redata=open('qiushibaikeRe.txt','r',encoding='utf8').read()
+    redata=open('xiushibaikeRe.txt','r',encoding='utf8').read()
     rst=re.compile(redata).findall(data)
-    with open('qiushibaike.txt', 'w',encoding='utf-8') as  f:
-        f.write('\n--------\n'.join('%s' % x for x in rst))
+    f.write('\n--------\n'.join('%s' % x for x in rst))
+f.close()
