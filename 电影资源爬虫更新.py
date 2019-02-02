0dba7019663810a531c3a5c2523bbeaa08ca40f5
jwmycz <924445155@qq.com>
2019-02-02 12:38:52 +0800

    Update 电影资源爬虫更新.py

--- 电影资源爬虫更新.py
+++ 电影资源爬虫更新.py
@@ -2,18 +2,16 @@ import requests
 import time
 import re
 from lxml import etree
+import seachmv
 heards = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
 urls=['http://www.okzy.co','http://www.1977zy.com','http://www.172zy.net/','http://www.605zy.com/','http://zuidazy.net/','http://131zy.net','http://api.172zy.xyz/','http://yongjiuzy.cc/','http://jingpinzy.com/','http://www.jingpinzy.net/']
 ###########################################################################################
 def starturl(url,i):
     newurl = []
-    #print(url)
     try:
         for i in range(1,i):
             starturl = url+'?m=vod-index-pg-'+str(i)+'.html'
-            #print(starturl)
             data = requests.get(starturl,timeout=1,headers=heards)
-            #print(data.text)
             mid = etree.HTML(data.text)
             linktry = mid.xpath('//ul//span[@class="xing_vb4"]/a/@href')
             linktry1=mid.xpath('//td[@class="l"]/a/@href')
@@ -21,11 +19,9 @@ def starturl(url,i):
                 link=linktry1
             else:
                 link=linktry
-            #print(link)
             for i in range(0, len(link)):
                 newurl.append(url + link[i])
     except Exception as err:
-        #print(err)
         print('主资源错误')
     return newurl
 ###########################################################################################
@@ -38,7 +34,6 @@ def everyurl(url):
         news=newstry1
     else:
         news=newstry
-    #print(news)
     remain='<h2>(.*?)</h2>'
     remain1='<!--片名开始-->(.*?)<!--片名结束-->'
     remain2='<li class="sa">.*?<!--.*?-->(.*?)<!--.*?--></li>'
@@ -47,9 +42,6 @@ def everyurl(url):
     name1try = re.compile(remain1, re.S).findall(data.text)
     name2try= re.compile(remain2, re.S).findall(data.text)
     name3try= re.compile(remain3, re.S).findall(data.text)
-    #print(nametry)
-    #print(name1try)
-    #print(name2try)
     if len(nametry)==0:
         if len(name1try)==0:
             if len(name2try)==0:
@@ -69,36 +61,35 @@ def everyurl(url):
                 i=sub1
             else:
                 i=new
-            file = open('.\\电影1资源\\' + i + '.txt', 'w')
+            file = open('.\\电影资源\\' + i + '.txt', 'w')
             file.write(now + '\n')
             for i in range(0, len(news)):
                 file.write(news[i] + '\n')
             file.close()
             print(name[j] + '    采集成功')
         except Exception as err:
-            #print('资源目录错误')
-            print(err)
+            print('资源目录错误')
 ##################################################################################
 def updata():
     url='https://raw.githubusercontent.com/chuyizh/demo/master/remand.txt'
     data=requests.get(url,headers=heards)
     x=int(data.text)
-    if (x==1):
+    if (x==2):
         print('软件为最新版本!')
-    elif (x==2):
-        #print('有新版本发布，请前往QQ群下载')
-        print('有新版本发布')
+    elif (x==3):
+        print('有新版本发布，请前往QQ群下载')
+        #print('有新版本发布')
     else:
         print('检查更新出错')
 ##################################################################################
 print('检查更新中......')
 time.sleep(2)
-#updata()
+updata()
+print('-------- by:将往\n QQ群:733127451')
+#print('-------- \n by:将往')
+print('采集模块加载中，请稍后\n')
+time.sleep(1)
 while True:
-    #print('-------- by:将往\n QQ群:733127451')
-    print('-------- \n by:将往\n ')
-    print('采集模块加载中，请稍后')
-    time.sleep(1)
     print('请选择采集方式\n1.全部采集  2.选择采集  3.搜索采集')
     x=int(input('请输入数字:'))
     if (x==1):
@@ -112,13 +103,11 @@ while True:
                         everyurl(nurl)
                     except Exception as err:
                         print('资源采集失败')
-                        #print(err)
             except Exception as err:
                 print('主资源错误')
-                #print(err)
         print('资源采集结束')
         print('采集功能完善中，如有问题请及时反馈')
-        print('                                   -----by:将往')
+        print('                        -----by:将往')
         time.sleep(2)
     elif x==2:
         print('请输入采集资源数：最大采集量为50000，每轮采集资源200，输入数字为2-400之间')
@@ -126,28 +115,34 @@ while True:
         for url in urls:
             try:
                 newurl=starturl(url,i)
-                print(newurl)
                 for nurl in newurl:
                     try:
-                        print(nurl)
                         everyurl(nurl)
                     except Exception as err:
                         print('资源采集失败')
-                        #print(err)
             except Exception as err:
                 print('主资源错误')
-                #print(err)
         print('资源采集结束')
         print('采集功能完善中，如有问题请及时反馈')
-        print('                                   -----by:将往')
+        print('                         -----by:将往')
         time.sleep(2)
     elif (x==3):
-        #print('本模块开发中,请使用其他功能。最新版本请加群下载QQ群:733127451')
-        print('本模块开发中,请使用其他功能。')
-        print('功能完善中，如有问题请及时反馈')
-        print('                                   -----by:将往')
+        txt = input('请输入要采集的内容：')
+        num = 0
+        try:
+            for url in urls:
+                allurl =seachmv.seach(url,txt)
+                for url in allurl:
+                    try:
+                        num = num + 1
+                        seachmv.sousuourl(url, num)
+                    except Exception as err:
+                        print('资源采集失败')
+        except Exception as err:
+            print('主资源错误')
+        print('资源采集结束')
+        print('采集功能完善中，如有问题请及时反馈')
+        print('                        -----by:将往')
         time.sleep(2)
     else:
         print('输入错误')
-
-
